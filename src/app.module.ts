39ee10fed704a89a93f81d1457624b6a08958333
Endyfadlullah <endinewjr68@gmail.com>
2024-08-14 15:40:21 +0700

    setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CommonModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
