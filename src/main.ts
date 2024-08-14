880e0587b960518de8bff89da23679a357fd77e3
muhamad fais aizat <muhfaisaizat@gmail.com>
2024-08-14 18:20:06 +0700

    istalasi package

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const logger = app.get (WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
+
+  const config = new DocumentBuilder()
+  .setTitle('Belajar NestAPI')
+  .setDescription('')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
